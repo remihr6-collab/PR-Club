0687c375433c9be1903baa83ab875c629d7ef5b0
Remi Harrar <remiharrar@macbook-pro-de-remi.home>
2026-03-24 00:54:49 +0100

    fix static html passthrough

--- .eleventy.js
+++ .eleventy.js
@@ -14,4 +14,29 @@ module.exports = function(eleventyConfig) {
       includes: "_includes"
     }
   };
+};module.exports = function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("style.css");
+  eleventyConfig.addPassthroughCopy("shared.js");
+  eleventyConfig.addPassthroughCopy("admin");
+  eleventyConfig.addPassthroughCopy("images");
+
+  // Copier les HTML statiques sans les traiter
+  eleventyConfig.addPassthroughCopy("index.html");
+  eleventyConfig.addPassthroughCopy("partenaires.html");
+  eleventyConfig.addPassthroughCopy("membre.html");
+  eleventyConfig.addPassthroughCopy("mission.html");
+  eleventyConfig.addPassthroughCopy("rejoindre.html");
+  eleventyConfig.addPassthroughCopy("partner-*.html");
+
+  eleventyConfig.addCollection("partenaires", function(collectionApi) {
+    return collectionApi.getFilteredByGlob("_posts/partenaires/*.md");
+  });
+
+  return {
+    dir: {
+      input: ".",
+      output: "_site",
+      includes: "_includes"
+    }
+  };
 };
