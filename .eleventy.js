8bccff999538cfd34f40634f1ffb71cdaf32e6fb
Remi Harrar <remiharrar@macbook-pro-de-remi.home>
2026-03-24 00:09:08 +0100

    add partenaires collection

--- .eleventy.js
+++ .eleventy.js
@@ -2,7 +2,11 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("style.css");
   eleventyConfig.addPassthroughCopy("shared.js");
   eleventyConfig.addPassthroughCopy("admin");
-  
+
+  eleventyConfig.addCollection("partenaires", function(collectionApi) {
+    return collectionApi.getFilteredByGlob("_posts/partenaires/*.md");
+  });
+
   return {
     dir: {
       input: ".",
